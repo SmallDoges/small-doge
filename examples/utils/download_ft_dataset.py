895b814cf3555feee6ad6692510c96d56eceb08a
LoserCheems <3314685395@qq.com>
2025-02-02 18:33:12 +0800

    Update dataset download script for R1

--- examples/utils/download_ft_dataset.py
+++ examples/utils/download_ft_dataset.py
@@ -13,11 +13,29 @@ def download_ultrafeedback_binarized(save_dir, cache_dir, num_proc):
     print(dataset)
     dataset.save_to_disk(save_dir + "/ultrafeedback_binarized", num_proc=num_proc)
 
+def download_bespoke_stratos(save_dir, cache_dir, num_proc):
+    # Download bespoke_stratos dataset
+    dataset = load_dataset("HuggingFaceH4/Bespoke-Stratos-17k", num_proc=num_proc, cache_dir=cache_dir)
+    print(dataset)
+    dataset.save_to_disk(save_dir + "/bespoke_stratos", num_proc=num_proc)
+
+def download_numinamath(save_dir, cache_dir, num_proc):
+    # Download numinamath dataset
+    dataset = load_dataset("AI-MO/NuminaMath-TIR", num_proc=num_proc, cache_dir=cache_dir)
+    print(dataset)
+    dataset.save_to_disk(save_dir + "/numinamath", num_proc=num_proc)
+
 # You can also download other datasets
 
 def main(args):
+
+    # For Instruction fine-tuning
     download_smoltalk(args.save_dir, args.cache_dir, args.num_proc)
     download_ultrafeedback_binarized(args.save_dir, args.cache_dir, args.num_proc)
+
+    # For Reasoning fine-tuning
+    download_bespoke_stratos(args.save_dir, args.cache_dir, args.num_proc)
+    download_numinamath(args.save_dir, args.cache_dir, args.num_proc)
 
 
 if __name__ == "__main__":
