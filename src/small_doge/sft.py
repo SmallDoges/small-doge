900cc39fcfedd3c90e0e3f1e5d10467d45dcb759
LoserCheems <3314685395@qq.com>
2025-02-17 00:19:11 +0800

    Fix padding bug in sft.py

--- src/small_doge/sft.py
+++ src/small_doge/sft.py
@@ -89,7 +89,7 @@ def main(script_args, training_args, model_args):
     tokenizer = AutoTokenizer.from_pretrained(
         model_args.model_name_or_path, trust_remote_code=model_args.trust_remote_code, use_fast=True
     )
-    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.padding_side = "right"
 
     ###################
     # Model init kwargs
